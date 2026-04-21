9d8433f2bb59c510404077fe8a651b16e5299ed8
irisortega499 <irisortega499@gmail.com>
2026-04-20 18:23:48 -0600

    Eliminación de registros

--- src/views/Categorias.jsx
+++ src/views/Categorias.jsx
@@ -7,6 +7,7 @@ import NotificacionOperacion from "../components/NotificacionOperacion";
 import TablaCategorias from "../components/categorias/TablaCategorias"; 
 import TarjetaCategoria from "../components/categorias/TarjetaCategoria"; 
 import ModalEdicionCategoria from "../components/categorias/ModalEdicionCategoria";
+import ModalEliminacionCategoria from "../components/categorias/ModalEliminacionCategoria"; 
 
 const Categorias = () => {
   const [toast, setToast] = useState({ mostrar: false, mensaje: "", tipo: "" });
@@ -182,6 +183,42 @@ const Categorias = () => {
     }
   };
 
+    const eliminarCategoria = async () => {
+    if (!categoriaAEliminar) return;
+    try {
+      setMostrarModalEliminacion(false);
+
+      const { error } = await supabase
+        .from("categorias")
+        .delete()
+        .eq("id_categoria", categoriaAEliminar.id_categoria);
+
+      if (error) {
+        console.error("Error al eliminar categoría:", error.message);
+        setToast({
+          mostrar: true,
+          mensaje: `Error al eliminar la categoría ${categoriaAEliminar.nombre_categoria}.`,
+          tipo: "error",
+        });
+        return;
+      }
+
+      await cargarCategorias();
+      setToast({
+        mostrar: true,
+        mensaje: `Categoría ${categoriaAEliminar.nombre_categoria} eliminada exitosamente.`,
+        tipo: "exito",
+      });
+    } catch (err) {
+      setToast({
+        mostrar: true,
+        mensaje: "Error inesperado al eliminar categoría.",
+        tipo: "error",
+      });
+      console.error("Excepción al eliminar categoría:", err.message);
+    }
+  };
+
   return (
     <Container className="mt-3">
       <Row className="align-items-center mb-3">
@@ -245,6 +282,13 @@ const Categorias = () => {
        categoriaEditar={categoriaEditar}
       manejoCambioInputEdicion={manejoCambioInputEdicion}
       actualizarCategoria={actualizarCategoria}
+      />
+
+      <ModalEliminacionCategoria
+        mostrarModalEliminacion={mostrarModalEliminacion}
+        setMostrarModalEliminacion={setMostrarModalEliminacion}
+        eliminarCategoria={eliminarCategoria}
+        categoria={categoriaAEliminar}
       />
       
       <NotificacionOperacion
