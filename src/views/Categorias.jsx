9f113f3b54e4407b774905c1a2224e0b6c8790ed
irisortega499 <irisortega499@gmail.com>
2026-04-20 18:02:22 -0600

    Actualización de registros

--- src/views/Categorias.jsx
+++ src/views/Categorias.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react"; 
-import { Container, Row, Col, Button, Spinner } from "react-bootstrap"; 
+import { Container, Row, Col, Button, Spinner, Modal } from "react-bootstrap"; 
 import { supabase } from "../database/supabaseconfig";
 
 import ModalRegistroCategoria from "../components/categorias/ModalRegistroCategoria";
 import NotificacionOperacion from "../components/NotificacionOperacion";
 import TablaCategorias from "../components/categorias/TablaCategorias"; 
 import TarjetaCategoria from "../components/categorias/TarjetaCategoria"; 
+import ModalEdicionCategoria from "../components/categorias/ModalEdicionCategoria";
 
 const Categorias = () => {
   const [toast, setToast] = useState({ mostrar: false, mensaje: "", tipo: "" });
@@ -37,6 +38,13 @@ const Categorias = () => {
     setMostrarModalEdicion(true);
   };
 
+    const manejoCambioInputEdicion = (e) => {
+    const { name, value } = e.target;
+    setCategoriaEditar((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
   const abrirModalEliminacion = (categoria) => {
     setCategoriaAEliminar(categoria);
     setMostrarModalEliminacion(true);
@@ -73,6 +81,54 @@ const Categorias = () => {
       [name]: value,
     }));
   };
+
+  const actualizarCategoria = async () => {
+    try {
+      if (
+        !categoriaEditar.nombre_categoria.trim() ||
+        !categoriaEditar.descripcion_categoria.trim()
+      ) {
+        setToast({
+          mostrar: true,
+          mensaje: "Debe llenar todos los campos.",
+          tipo: "advertencia",
+        });
+        return;
+      }
+      setMostrarModalEdicion(false);
+      const { error } = await supabase
+        .from("categorias")
+        .update({
+          nombre_categoria: categoriaEditar.nombre_categoria,
+          descripcion_categoria: categoriaEditar.descripcion_categoria,
+        })
+        .eq("id_categoria", categoriaEditar.id_categoria);
+
+      if (error) {
+        console.error("Error al actualizar categoría:", error.message);
+        setToast({  
+          mostrar: true,
+          mensaje: "Error al actualizar la categoría. ${categoriaEditar.nombre_categoria}.",
+          tipo: "error",
+        });
+        return;
+      }
+      await cargarCategorias();
+      setToast({
+        mostrar: true,
+        mensaje: "Categoría ${categoriaEditar.nombre_categoria} actualizada exitosamente.",
+        tipo: "exito",
+      });
+    } catch (err) {
+      setToast({
+        mostrar:true,
+        mensaje: "Error inesperado al actualizar la categoría.",
+        tipo: "error",
+      });
+      console.error("Excepción al actualizar categoría:", err.message);
+    }
+  };
+
 
   const agregarCategoria = async () => {
     try {
@@ -181,6 +237,14 @@ const Categorias = () => {
         nuevaCategoria={nuevaCategoria}
         manejoCambioInput={manejoCambioInput}
         agregarCategoria={agregarCategoria}
+      />
+
+      <ModalEdicionCategoria
+       mostrarModalEdicion={mostrarModalEdicion}
+       setMostrarModalEdicion={setMostrarModalEdicion}
+       categoriaEditar={categoriaEditar}
+      manejoCambioInputEdicion={manejoCambioInputEdicion}
+      actualizarCategoria={actualizarCategoria}
       />
       
       <NotificacionOperacion
